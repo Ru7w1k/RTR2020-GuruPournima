3a852a2f1406facde8c7f89310dfd0c16889e586
Rahul Borkar <rborkar@plex.com>
2020-07-05 21:19:10 +0530

    Added flowers below chair

---
+++ Flowers.h
@@ -0,0 +1,112 @@
+#include<GL\freeglut.h>
+
+void DisplayFlower()
+{  
+    glLineWidth(1.5);
+    
+    float length = 0.10f;    
+
+    int i = 0, noOfEdges = 4;
+    float xChange=0;
+    float yChange=0;
+
+    float lineStartX, lineStartY, lineEndX, lineEndY;
+
+
+    float x = -0.60f;
+    float y = -0.60f;
+    
+    glBegin(GL_LINES);
+	    glColor3f(0.278f,0.157f,1.00f);      
+     
+
+         glVertex2f(x + 0.0, y + 0.20f);
+         glVertex2f(x + 0.0, y + (-0.20f) );     
+
+         glVertex2f(x + 0.075f, y + 0.18f);
+         glVertex2f(x + (-0.075), y + (-0.18));  
+
+         glVertex2f(x+0.15, y+0.15);
+         glVertex2f(x+-0.15,y+ -0.15);    
+
+         glVertex2f(x+0.25, y+0.10);
+         glVertex2f(x+-0.25,y+ -0.10);    
+
+         glVertex2f(x+0.25,  y+0.0);
+         glVertex2f(x+-0.25, y+ 0.0);  
+
+         glVertex2f(x+-0.23,y+ 0.058);
+         glVertex2f(x+0.23, y+-0.058);  
+         
+         glVertex2f(x+-0.075,y+ 0.18);
+         glVertex2f(x+0.075, y+-0.18);
+         
+         glVertex2f(x+-0.15,y+ 0.15);
+         glVertex2f(x+0.15, y+-0.15);
+    glEnd();
+
+    x = 0.0f;
+    y = -0.90f;
+    
+    glBegin(GL_LINES);
+	    glColor3f(0.278f,0.157f,1.00f);      
+     
+
+         glVertex2f(x + 0.0, y + 0.20f);
+         glVertex2f(x + 0.0, y + (-0.20f) );     
+
+         glVertex2f(x + 0.075f, y + 0.18f);
+         glVertex2f(x + (-0.075), y + (-0.18));  
+
+         glVertex2f(x+0.15, y+0.15);
+         glVertex2f(x+-0.15,y+ -0.15);    
+
+         glVertex2f(x+0.25, y+0.10);
+         glVertex2f(x+-0.25,y+ -0.10);    
+
+         glVertex2f(x+0.25,  y+0.0);
+         glVertex2f(x+-0.25, y+ 0.0);  
+
+         glVertex2f(x+-0.23,y+ 0.058);
+         glVertex2f(x+0.23, y+-0.058);  
+         
+         glVertex2f(x+-0.075,y+ 0.18);
+         glVertex2f(x+0.075, y+-0.18);
+         
+         glVertex2f(x+-0.15,y+ 0.15);
+         glVertex2f(x+0.15, y+-0.15);
+    glEnd();
+
+
+    x = 0.60f;
+    y = -0.60f;
+    
+    glBegin(GL_LINES);
+	    glColor3f(0.278f,0.157f,1.00f);      
+     
+
+         glVertex2f(x + 0.0, y + 0.20f);
+         glVertex2f(x + 0.0, y + (-0.20f) );     
+
+         glVertex2f(x + 0.075f, y + 0.18f);
+         glVertex2f(x + (-0.075), y + (-0.18));  
+
+         glVertex2f(x+0.15, y+0.15);
+         glVertex2f(x+-0.15,y+ -0.15);    
+
+         glVertex2f(x+0.25, y+0.10);
+         glVertex2f(x+-0.25,y+ -0.10);    
+
+         glVertex2f(x+0.25,  y+0.0);
+         glVertex2f(x+-0.25, y+ 0.0);  
+
+         glVertex2f(x+-0.23,y+ 0.058);
+         glVertex2f(x+0.23, y+-0.058);  
+         
+         glVertex2f(x+-0.075,y+ 0.18);
+         glVertex2f(x+0.075, y+-0.18);
+         
+         glVertex2f(x+-0.15,y+ 0.15);
+         glVertex2f(x+0.15, y+-0.15);
+    glEnd();
+}
