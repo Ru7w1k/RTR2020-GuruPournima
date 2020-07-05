a43e5451e710683f85e6079360192eb3c9b3b65d
Rutwik Choughule <rutwik110@gmail.com>
2020-07-05 15:45:34 +0530

    right wall adjustment

--- RightWall.h
+++ RightWall.h
@@ -2,12 +2,10 @@
 
 
 void DisplayRightWall(float roomWidth, float roomHeight){
-    //glClear(GL_COLOR_BUFFER_BIT);
-//	glMatrixMode(GL_MODELVIEW);
-//	glLoadIdentity();
+
 	glBegin(GL_QUADS);
 	glColor3f(0.92f, 0.92f, 0.75f);
-	glVertex3f(0.3 * roomWidth, 0.35 * roomHeight, 0.0f);
+	glVertex3f(0.3 * roomWidth, 0.425 * roomHeight, 0.0f);
 
 	glColor3f(1.0f, 1.0f, 0.8f);
 	glVertex3f(roomWidth/2, roomHeight / 2, 0.0f);
